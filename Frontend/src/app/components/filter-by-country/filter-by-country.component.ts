31a0483c36bb9da24951d635bcad34021a9acc26
Fabio Costa <gurzo94@hotmail.it>
2020-06-28 17:45:22 +0200

    grafico spostato in filter-by-country

--- Frontend/src/app/components/filter-by-country/filter-by-country.component.ts
+++ Frontend/src/app/components/filter-by-country/filter-by-country.component.ts
@@ -4,6 +4,9 @@ import { CovidData } from '../../models/data.model';
 import { FormControl } from '@angular/forms';
 import { Observable } from 'rxjs';
 import { map, startWith } from 'rxjs/operators';
+import { ChartDataSets, ChartOptions } from 'chart.js';
+import { Color, Label } from 'ng2-charts';
+
  
 @Component({
   selector: 'app-filter-by-country',
@@ -13,11 +16,37 @@ import { map, startWith } from 'rxjs/operators';
 export class FilterByCountryComponent implements OnInit {
  
   constructor(private dataService:DataService) { }
+
+  //per il grafico
+  lineChartData: ChartDataSets[] = [
+    // { data: [85, 72, 78, 75, 77, 75, 0,0,0,0,0,0], label: 'Casi Coronavirus' },
+    {data: 
+      [5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 100],
+       label: 'Casi coronavirus'},
+  ];
+
+  lineChartLabels: Label[] = 
+  ['Gennaio', 'Febbraio', 'Marzo', 'Aprile', 'Maggio', 'Giugno','Luglio','Agosto','Settembre','Ottobre','Novembre','Dicembre'];
+
+  lineChartOptions = {
+    responsive: true,
+  };
+
+  lineChartColors: Color[] = [
+    {
+      borderColor: 'black',
+      backgroundColor: 'rgba(255,255,0,0.28)',
+    },
+  ];
+
+  lineChartLegend = true;
+  lineChartPlugins = [];
+  lineChartType = 'line';
  
-  myControl = new FormControl();
-  countries : string[] = new Array();
-  filteredOptions: Observable<string[]>;
-  public covidData : CovidData [];
+  myControl = new FormControl(); //per far funzionare il filtro
+  countries : string[] = new Array(); // per il filtro dinamico
+  filteredOptions: Observable<string[]>; //per il filtro
+  public covidData : CovidData []; //collegamento al database
  
  
   public country:string; //per memorizzare la stringa dell’input
@@ -27,21 +56,22 @@ export class FilterByCountryComponent implements OnInit {
     return this.dataService.getData().subscribe( (response : CovidData[]) => {
       this.covidData = response;
       response.forEach(item => {
-        this.countries.push(item.country);
-        console.log(this.countries);
+        this.countries.push(item.country); //pusha nel vettore i dati di country
+        console.log(this.countries); //check
       })
      
     })
   }
   ngOnInit() {
-    this.getEntries()
+    this.getEntries() //filtro
     this.filteredOptions = this.myControl.valueChanges
-      .pipe(
+      .pipe(  //filtro dimanico
         startWith(''),
         map(value => this._filter(value))
       );
   }
  
+  //funzione per il filtro dinamico
   private _filter(value: string): string[] {
     const filterValue = value.toLowerCase();
  
