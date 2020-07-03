009f235ace43689891fe37f1a7ee0cfd307e2038
simonaester <simonaester@hotmail.it>
2020-07-03 15:43:20 +0200

    login finito e funzionante!

--- Frontend/src/app/routes/login/login.component.ts
+++ Frontend/src/app/routes/login/login.component.ts
@@ -5,47 +5,41 @@ import { Router, ActivatedRoute } from '@angular/router';
 import { UsersData } from '../../models/data.model';
 
 @Component({
-selector: 'app-login',
-templateUrl: './login.component.html',
-styleUrls: ['./login.component.css']
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
 
-constructor(private usersService: UsersService, private router: Router) { }
+  constructor(private usersService: UsersService, private router: Router) { }
 
-public usersData: UsersData [];
-public usersEntry: UsersData;
+  public usersData: UsersData[];
+  public usersEntry: UsersData;
 
-ngOnInit(): void {
+  ngOnInit(): void {
 
-this.fetchEntry();
+    this.fetchEntry();
   }
-  fetchEntry(){
-    this.usersService.getData().subscribe( (res: any ) => {
+  fetchEntry() {
+    this.usersService.getData().subscribe((res: any) => {
       this.usersData = res;
-      console.log("valori db:",this.usersData);
+      console.log("valori db:", this.usersData);
     })
   }
 
-  onSubmit(f: NgForm){
-  this.usersEntry = f.form.value;
-  this.usersEntry.permissions=1&&2&&3;
-  console.log(this.usersEntry);
+  onSubmit(f: NgForm) {
+    this.usersEntry = f.form.value;
 
-   let j=0;
-    for(let i=0;i<this.usersData.length;i++){
-      if (this.usersEntry.username==this.usersData[i].username&&
-        this.usersEntry.password==this.usersData[i].password&&
-          this.usersEntry.permissions==this.usersData[i].permissions) 
-        console.log(" is logged in",this.usersEntry.username)
-      for(j=0;j<this.usersData.length;i++){
-        if (this.usersEntry.username!=this.usersData[i].username&&
-          this.usersEntry.password!=this.usersData[i].password&&
-            this.usersEntry.permissions!=this.usersData[i].permissions) 
-            console.log("users doesn't exist")
-        }
-      
-      }
-      }
+    console.log(this.usersEntry);
+
+    for (let i = 0; i < this.usersData.length; i++) {
+      if (this.usersEntry.username == this.usersData[i].username &&
+        this.usersEntry.password == this.usersData[i].password) {
+        console.log(this.usersEntry.username, " is logged in")
+      } else
+        console.log("users doesn't exist")
+    }
   }
+}
+
 
